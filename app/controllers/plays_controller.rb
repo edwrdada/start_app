a2a664514342335c39079d56fd61106eb093be58
Nao Ada <0911adachi@gmail.com>
2019-03-04 02:04:12 +0900

    start all

---
+++ app/controllers/plays_controller.rb
@@ -0,0 +1,16 @@
+class PlaysController < ApplicationController
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def top
+  end
+
+  def login
+  end
+end
---
+++ db/migrate/20190303165607_create_plays.rb
@@ -0,0 +1,10 @@
+class CreatePlays < ActiveRecord::Migration[5.2]
+  def change
+    create_table :plays do |t|
+      t.string :name
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
